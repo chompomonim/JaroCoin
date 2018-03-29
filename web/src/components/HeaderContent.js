d2ea6b80ce8e2371587c70ca1cff921a216acd2f
humankindmeta <humankindmeta@gmail.com>
2018-03-29 22:48:51 +0300

    Adding TimeBar & Changing text in 'What Is JaroCoin?' - timeBar now displays time in miliseconds. 3 last numbers need to be sliced

--- web/src/components/HeaderContent.js
+++ web/src/components/HeaderContent.js
@@ -6,6 +6,8 @@ import { Divider } from 'antd'
 
 import TypistComponent from './TypistComponent'
 import CountdownTimer from './CountdownTimer'
+import TimeBar from './TimeBar'
+import GetJaroButton from './GetJaroButton'
 
 import styles from './headerContent.module.css'
 
@@ -31,18 +33,28 @@ class HeaderContent extends Component {
                          <CountdownTimer />               
                     </Col>
                 </Row>
+
+                <Row className={styles.timeBarElementsWrapper} gutter={20} type="flex">
+                    <Col className={styles.timeBarColumnLeft} xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 20 }} lg={{ span: 20 }}>
+                        <TimeBar />
+                    </Col>
+                    <Col className={styles.timeBarColumnRight} xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 4 }} lg={{ span: 4 }}>
+                        <div className={styles.getJaroButtonContainer}>
+                            <GetJaroButton  />
+                        </div>
+                    </Col>
+                </Row>
+
+                <Divider className={styles.divider} />
+
                 <Row className={styles.introWrapper} gutter={20} type="flex">
                     <Col className={styles.introColumnCenter} xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 18 }} lg={{ span: 18 }}>
                         <h3 className={styles.introSectionTitle}>INTRO</h3>
                         <p className={styles.introDescription}>JaroCoin is a token fully backes by my most precious asset – time. JaroCoin is the only way to get my attention, advice or service.</p>
                         <p className={styles.introDescription}>Private crypto currency (token) is a model to play for majority future – oriented professionals from attorneys at law to house – keepers. This project is fully open source, so you can create yours as well!</p>
                         <p className={styles.introDescription}>Humanity changes. Money too.</p>
-                    </Col>
-                </Row>
-                <Row className={styles.introButtonWrapper} gutter={20} type="flex">
-                    <Col className={styles.columnCenter} xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 5 }} lg={{ span: 5 }}> 
                         <div className={styles.buttonWrapper}>
-                            <Button className={styles.button} type="primary" size="large" icon="book">Download White Paper</Button>
+                            <Button className={styles.button} type="primary" size="large" icon="book">Read Whitepaper</Button>
                         </div>
                     </Col>
                 </Row>
---
+++ web/src/components/TimeBar.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+
+import { Progress } from 'antd'
+
+import Countdown from 'react-countdown-now';
+
+import styles from './timeBar.module.css'
+
+
+const Completionist = () => <span>You are good to go!</span>
+
+// Renderer callback with condition
+const renderer = ({ total, completed }) => {
+    if (completed) {
+        return <Completionist />;
+    } else {
+        // Render a countdown
+        // 10 Years = 315619200000 Miliseconds
+        return (
+            <div className={styles.timeBarWrapper}>
+                <div className={styles.timeBarNumber}>{total}</div>
+                <div className={styles.timeBarTitle}> miliseconds left</div><br />
+                <Progress className={styles.progressBar} percent={Math.floor((total / 325619200000) * 100)} showInfo={true} strokeWidth="90px" />
+            </div>
+            
+        );
+    }
+};
+
+
+class TimeBar extends Component {
+    render() {
+        return (
+            <Countdown
+                intervalDelay={50}
+                precision={3}
+                // date={Date.now() + 5000} // Quick test
+                date={'Sat, 01 Apr 2028 12:00:00'}
+                renderer={renderer}
+            />
+        );
+    }
+}
+
+export default TimeBar
