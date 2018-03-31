86cd6a995fa8121cf1da4adbebbeced8a03c7033
humankindmeta <humankindmeta@gmail.com>
2018-03-31 20:01:02 +0300

    Mobile view for 'Quotes'

--- web/src/components/Quotes.js
+++ web/src/components/Quotes.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
 
 import { Carousel } from 'antd'
+import { Row, Col } from 'antd'
 import { Icon } from 'antd'
+
+import MediaQuery from 'react-responsive'
 
 import styles from './quotes.module.css'
 
@@ -11,26 +14,52 @@ class Quotes extends Component {
         return (
             <div id="quotes" className={styles.quotesWrapper}>
                 <h2 className={styles.quotesHeading}>Quotes</h2>
+                <MediaQuery query="(min-width: 768px)">
+                    <Carousel
+                        // Props: https://github.com/akiran/react-slick
+                        autoplay
+                        className={styles.carousel}
+                        // arrows={true}
+                        autoplaySpeed={'4000'}
+                        draggable={true}
+                        pauseOnHover={true}
+                        effect={'fade'}
+                        // centerMode={true}
+                    >
+                        <div className={styles.carouselCard}>
+                            <div className={styles.carouselCardText}>"One of the smartest men I've ever met"</div>
+                            <div className={styles.carouselCardAuthor}>Elon Musk, founder fo Tesla</div>
+                        </div>
+                        <div className={styles.carouselCard}>
+                            <div className={styles.carouselCardText}>"This person, helped me to change my thinking"</div>
+                            <div className={styles.carouselCardAuthor}>Mark Zuckerberg, founder fo Facebook</div>
+                        </div>
 
-                <Carousel
-                    // Props: https://github.com/akiran/react-slick
-                    autoplay
-                    className={styles.carousel}
-                    // arrows={true}
-                    autoplaySpeed={'3000'}
-                    draggable={true}
-                    pauseOnHover={true}
-                // centerMode={true}
-                >
-                    <div className={styles.carouselCard}>
-                        <div className={styles.text}>"One of the smartest men I've ever met"</div>
-                        <div className={styles.author}>Elon Musk, founder fo Tesla</div>
-                    </div>
-                    <div className={styles.carouselCard}>
-                        <div className={styles.text}>"This person, helped me to change my thinking"</div>
-                        <div className={styles.author}>Mark Zuckerberg, founder fo Facebook</div>
-                    </div>
-                </Carousel>
+                    </Carousel>
+                </MediaQuery>
+
+                <MediaQuery query="(max-width: 767px)">
+                    <Carousel
+                        // Props: https://github.com/akiran/react-slick
+                        autoplay
+                        className={styles.carousel}
+                        // arrows={true}
+                        autoplaySpeed={'4000'}
+                        draggable={true}
+                        pauseOnHover={true}
+                        effect={'fade'}
+                        // centerMode={true}
+                    >
+                        <div className={styles.carouselCard}>
+                            <div className={styles.carouselCardTextMobile}>"One of the smartest men I've ever met"</div>
+                            <div className={styles.carouselCardAuthorMobile}>Elon Musk, founder fo Tesla</div>
+                        </div>
+                        <div className={styles.carouselCard}>
+                            <div className={styles.carouselCardTextMobile}>"This person, helped me to change my thinking"</div>
+                            <div className={styles.carouselCardAuthorMobile}>Mark Zuckerberg, founder fo Facebook</div>
+                        </div>
+                    </Carousel>
+                </MediaQuery>
             </div>
         );
     }
