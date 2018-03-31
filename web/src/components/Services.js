b343e40c9325c149605df3fee3a325d2a37a3e87
humankindmeta <humankindmeta@gmail.com>
2018-03-31 18:30:54 +0300

    Reverting 'Services' Mobile view with some fix'es

--- web/src/components/Services.js
+++ web/src/components/Services.js
@@ -17,40 +17,38 @@ class Services extends Component {
 
     render() {
         return (
-            <div>
-                <Row className={styles.servicesWrapper} gutter={48} type="flex" justify="center" align="middle">
-                    <Col className={styles.servicesHeading} span={24}>
-                        <h2>Services</h2>
+            <div id="services" className={styles.servicesWrapper}>
+                <h2 className={styles.servicesHeading}>Services</h2>
+                <Row className={styles.servicesItemListWrapper} gutter={48} type="flex" justify="center">
+                    <Col xs={{ span: 22 }} sm={{ span: 22 }} md={{ span: 24 }} lg={{ span: 24 }}>
+                        <h3 className={styles.servicesSubtitle}>With JaroCoin You Can</h3> 
                     </Col>
-                    <Col className={styles.servicesSubtitle} span={24}>
-                        <h3>With JaroCoin You Can</h3>
-                    </Col>
-                    <Col className={styles.servicesItemWrapper} xs={{ span: 22 }} sm={{ span: 12 }} md={{ span: 12 }} lg={{ span: 8 }}>
+                    <Col className={styles.servicesItemWrapper} xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 12 }} lg={{ span: 8 }}>
                         <Icon type="line-chart" className={styles.servicesItemIcon} />
                         <h3 className={styles.servicesItemTitle}>Trade</h3>
                         <p className={styles.servicesItemDescription}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem. </p>
                     </Col>
-                    <Col className={styles.servicesItemWrapper} xs={{ span: 22 }} sm={{ span: 12 }} md={{ span: 12 }} lg={{ span: 8 }}>
+                    <Col className={styles.servicesItemWrapper} xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 12 }} lg={{ span: 8 }}>
                         <Icon type="bar-chart" className={styles.servicesItemIcon} />
                         <h3 className={styles.servicesItemTitle}>HODL</h3>
                         <p className={styles.servicesItemDescription}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem. </p>
                     </Col>
-                    <Col className={styles.servicesItemWrapper} xs={{ span: 22 }} sm={{ span: 12 }} md={{ span: 12 }} lg={{ span: 8 }}>
+                    <Col className={styles.servicesItemWrapper} xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 12 }} lg={{ span: 8 }}>
                         <Icon type="code-o" className={styles.servicesItemIcon} />
                         <h3 className={styles.servicesItemTitle}>Ask to Code</h3>
                         <p className={styles.servicesItemDescription}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem. </p>
                     </Col>
-                    <Col className={styles.servicesItemWrapper} xs={{ span: 22 }} sm={{ span: 12 }} md={{ span: 12 }} lg={{ span: 8 }}>
+                    <Col className={styles.servicesItemWrapper} xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 12 }} lg={{ span: 8 }}>
                         <Icon type="notification" className={styles.servicesItemIcon} />
                         <h3 className={styles.servicesItemTitle}>Get Crypto Advice</h3>
                         <p className={styles.servicesItemDescription}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem. </p>
                     </Col>
-                    <Col className={styles.servicesItemWrapper} xs={{ span: 22 }} sm={{ span: 12 }} md={{ span: 12 }} lg={{ span: 8 }}>
+                    <Col className={styles.servicesItemWrapper} xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 12 }} lg={{ span: 8 }}>
                         <Icon type="shopping-cart" className={styles.servicesItemIcon} />
                         <h3 className={styles.servicesItemTitle}>Pay for Webinar</h3>
                         <p className={styles.servicesItemDescription}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem. </p>
                     </Col>
-                    <Col className={styles.servicesLastItemWrapper} xs={{ span: 22 }} sm={{ span: 12 }} md={{ span: 12 }} lg={{ span: 8 }}>
+                    <Col className={styles.servicesLastItemWrapper} xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 12 }} lg={{ span: 8 }}>
                         <Button className={styles.servicesLearnMoreButton} type="dashed" size="large" onClick={() => this.setModalVisible(true)}>
                             Learn More
                         </Button>
