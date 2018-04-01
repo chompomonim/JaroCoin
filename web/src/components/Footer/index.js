2578fc94aa066a82631dbe45d40636d7fec312d9
Jaro <jaro@nous.lt>
2018-04-02 00:10:35 +0300

    More links in footer

--- web/src/components/Footer/index.js
+++ web/src/components/Footer/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import { Col, Row, Button, Icon, Modal } from 'antd'
+import { Col, Row, Icon, Modal } from 'antd'
 //
 import MediaQuery from 'react-responsive'
 
@@ -69,8 +69,8 @@ class Footer extends Component {
                         <Col className={styles.footerLastColumn} md={{ span: 8 }} lg={{ span: 8 }}>
                             <a className={styles.footerItemLink} href="https://medium.com/@chompomonim/" target="_blank" rel="noopener noreferrer">Blog</a><br />
                             <a className={styles.footerItemLink} href="#faq" target="_blank" rel="noopener noreferrer">FAQ</a><br />
-                            <a className={styles.footerItemLink} href="#" target="_blank" rel="noopener noreferrer">Telegram group</a><br />
-                            <a className={styles.footerItemLink} href="https://youtube.com/" target="_blank" rel="noopener noreferrer">Youtube Channel</a><br />
+                            <a className={styles.footerItemLink} href="https://t.me/JaroCoin" target="_blank">Telegram group!</a><br />
+                            <a className={styles.footerItemLink} href="https://www.youtube.com/channel/UCv_w5WZCRtyRlPyTymqt8Rw" target="_blank">Youtube Channel</a><br />
                         </Col>
                     </Row>
                 </MediaQuery>
@@ -83,7 +83,7 @@ class Footer extends Component {
                             <a className={styles.footerItemLink} href="#token-distribution-model" rel="noopener noreferrer">Token distribution model</a><br />
                         </Col>
                         <Col className={styles.footerMobileColumn} xs={{ span: 24 }} sm={{ span: 8 }}>
-                            <a className={styles.footerItemLink} href="#about" rel="noopener noreferrer">Jaro Skills</a><br />
+                            <a className={styles.footerItemLink} href="https://linkedin.com/in/jarolt" rel="noopener noreferrer" target="_blank">Jaro Skills</a><br />
                             <a className={styles.footerItemLink} href="#services" rel="noopener noreferrer">Services</a><br />
                             <a className={styles.footerItemLink} href="#" target="_blank" rel="noopener noreferrer">Calendar</a><br />
                             <a className={styles.footerItemLink} href="#" target="_blank" rel="noopener noreferrer">WhitePaper</a><br />
@@ -91,8 +91,8 @@ class Footer extends Component {
                         <Col className={styles.footerMobileColumn} xs={{ span: 24 }} sm={{ span: 12 }}>
                             <a className={styles.footerItemLink} href="https://medium.com/@chompomonim/" target="_blank" rel="noopener noreferrer">Blog</a><br />
                             <a className={styles.footerItemLink} href="#faq" target="_blank" rel="noopener noreferrer">FAQ</a><br />
-                            <a className={styles.footerItemLink} href="https://t.me/JaroCoin" target="_blank" rel="noopener noreferrer">Telegram group</a><br />
-                            <a className={styles.footerItemLink} href="https://youtube.com/" target="_blank" rel="noopener noreferrer">Youtube Channel</a><br />
+                            <a className={styles.footerItemLink} href="https://t.me/JaroCoin" target="_blank">Telegram group!</a><br />
+                            <a className={styles.footerItemLink} href="https://www.youtube.com/channel/UCv_w5WZCRtyRlPyTymqt8Rw" target="_blank">Youtube Channel</a><br />
                         </Col>
                     </Row>
                 </MediaQuery>
@@ -103,25 +103,25 @@ class Footer extends Component {
                         <a className={styles.footerTermsItem} onClick={disclaimerModal}>Disclaimer</a>
                     </Col>
                     <Col className={styles.footerSocialWrapper} xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 10, pull: 1 }} lg={{ span: 8}}>
-                        <a className={styles.footerSocialItem} style={{ color: '#1d1d1d' }} href="mailto:jaro@jarocoin.com?Subject=JaroCoin%20Web%20Contact" target="_top" rel="noopener noreferrer">
+                        <a className={styles.footerSocialItem} style={{ color: '#1d1d1d' }} href="mailto:jaro@jarocoin.com?Subject=JaroCoin%20Web%20Contact" target="_top">
                             <Icon type="mail" />
                         </a>
-                        <a className={styles.footerSocialItem} style={{ color: '#1d1d1d' }} href="https://twitter.com/chompomonim" target="_blank" rel="noopener noreferrer">
+                        <a className={styles.footerSocialItem} style={{ color: '#1d1d1d' }} href="https://twitter.com/chompomonim" target="_blank">
                             <Icon type="twitter" /> {/*color: '#37c6f5'*/}
                         </a>
-                        <a className={styles.footerSocialItem} style={{ color: '#1d1d1d' }} href="https://plus.google.com/+JaroSatkevic" target="_blank" rel="noopener noreferrer">
+                        <a className={styles.footerSocialItem} style={{ color: '#1d1d1d' }} href="https://plus.google.com/+JaroSatkevic" target="_blank">
                             <Icon type="google-plus" /> {/* color: '#dd4d42' */}
                         </a>
-                        <a className={styles.footerSocialItem} style={{ color: '#1d1d1d' }} href="https://medium.com/@chompomonim/" target="_blank" rel="noopener noreferrer">
+                        <a className={styles.footerSocialItem} style={{ color: '#1d1d1d' }} href="https://medium.com/@chompomonim/" target="_blank">
                             <Icon type="medium" /> {/* color: '#000000' */}
                         </a>
-                        <a className={styles.footerSocialItem} style={{ color: '#1d1d1d' }} href="https://www.facebook.com/chompomonim" target="_blank" rel="noopener noreferrer">
+                        <a className={styles.footerSocialItem} style={{ color: '#1d1d1d' }} href="https://www.facebook.com/chompomonim" target="_blank">
                             <Icon type="facebook" /> {/* color: '#4767aa' */}
                         </a>
-                        <a className={styles.footerSocialItem} style={{ color: '#1d1d1d' }} href="https://www.linkedin.com/in/jarolt" target="_blank" rel="noopener noreferrer">
+                        <a className={styles.footerSocialItem} style={{ color: '#1d1d1d' }} href="https://www.linkedin.com/in/jarolt" target="_blank">
                             <Icon type="linkedin" /> {/* color: '#0077b5 */}
                         </a>
-                        <a className={styles.footerSocialItem} style={{ color: '#1d1d1d' }} href="https://youtube.com/" target="_blank" rel="noopener noreferrer">
+                        <a className={styles.footerSocialItem} style={{ color: '#1d1d1d' }} href="https://www.youtube.com/channel/UCv_w5WZCRtyRlPyTymqt8Rw" target="_blank" rel="noopener noreferrer">
                             <Icon type="youtube" /> {/* color: '#ff0000' */}
                         </a>
                     </Col>
